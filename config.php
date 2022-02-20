193dc9965f25e6369fb63c76f38f0bacd057fc84
sevvallk <57112925+sevvallk@users.noreply.github.com>
2022-02-20 15:18:16 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$open = mysqli_connect("localhost", "root", "", "blabla", 3306);
+$open = mysqli_connect("127.0.0.1", "root", "", "blabla", 3306);
 if ($open == FALSE) {
     exit(json_encode(["result" => NULL, "error" => ["code" => NULL, "message" => "An error occurred while starting new or resuming existing session."]]));
 }
